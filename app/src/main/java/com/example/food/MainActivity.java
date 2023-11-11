c9db6095283c72491a9d75248b2d9b5a3b9759b6
priya-zha <pj35134@gmail.com>
2023-11-11 16:02:25 -0600

    uploaded sqlite files

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -18,19 +18,63 @@ import com.example.food.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private MyDatabaseHelper dbHelper;
+    private EditText editTextUsername, editTextEmail, editTextPassword;
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+                dbHelper = new MyDatabaseHelper(this);
+
+                editTextUsername = findViewById(R.id.editTextUsername);
+                editTextEmail = findViewById(R.id.editTextEmail);
+                editTextPassword = findViewById(R.id.editTextPassword);
+
+                buttonRegister = findViewById(R.id.buttonRegister);
+                buttonRegister.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        registerUser();
+                    }
+                });
+            }
+
+            private void registerUser() {
+                String username = editTextUsername.getText().toString().trim();
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
+                    long newRowId = dbHelper.insertUser(username, email, password);
+
+                    if (newRowId != -1) {
+                        showToast("User registered successfully with ID: " + newRowId);
+                        // Clear input fields after successful registration
+                        editTextUsername.getText().clear();
+                        editTextEmail.getText().clear();
+                        editTextPassword.getText().clear();
+                    } else {
+                        showToast("Error registering user.");
+                    }
+                } else {
+                    showToast("Please fill in all fields.");
+                }
+            }
+
+            private void showToast(String message) {
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            }
+        }
 
 
-    }
-}
---
+++ app/src/main/java/com/example/food/MyDatabaseHelper.java
@@ -0,0 +1,55 @@
+package com.example.food;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "UserDatabase";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NAME = "users";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_USERNAME = "username";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_PASSWORD = "password";
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_USERNAME + " TEXT, " +
+                    COLUMN_EMAIL + " TEXT, " +
+                    COLUMN_PASSWORD + " TEXT);";
+
+    public MyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public long insertUser(String username, String email, String password) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USERNAME, username);
+        values.put(COLUMN_EMAIL, email);
+        values.put(COLUMN_PASSWORD, password);
+        long newRowId = db.insert(TABLE_NAME, null, values);
+        db.close();
+        return newRowId;
+    }
+    }
+
+
